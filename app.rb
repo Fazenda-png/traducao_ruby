3b6e3f6a7fcedc2f078726d103799183896eb882
Fazenda-jpg <fazenda.j.p.s@hotmail.com>
2020-12-07 13:48:51 -0300

    Testando o cod

--- app.rb
+++ app.rb
@@ -1,14 +1,16 @@
 require_relative "traducao"
 
+## lista de idiomas disponibilizados pela API
 list_idioma = {1 => 'PtBr', 2 => 'EnUs', 3 => 'ZhCh', 4 => 'DeAl'}
 
+## Menu 
 puts('---- Menu ----')
 puts('1 - Traduzir')
 puts('2 - Sair')
 
 option = gets.chomp.to_i
 
-
+## If para pegar os dados para o metodo "Traduzir"
 if option == 1
     puts('Lista de Idiomas:')
     puts('1 - Português do Brasil')
@@ -16,12 +18,12 @@ if option == 1
     puts('3 - Chinês (Simplificado)')
     puts('4 - Alemão')
 
-    print("Digite a mensagem: ")
-    mensagem = gets.chomp.to_s
-
-    print("Em qual idioma a mensagem está escrita?: ")
+    print("Qual o idioma que você deseja escrever sua mensagem?: ")
     numero_atual = gets.chomp.to_i
     idioma_atual = list_idioma[numero_atual]
+
+    print("Digite a mensagem: ")
+    mensagem = gets.chomp.to_s
 
     print("Traduzir para: ")
     numero_traduz = gets.chomp.to_i
--- traducao.rb
+++ traducao.rb
@@ -3,38 +3,34 @@ require 'uri'
 require 'json'
 
 class Traducao
+    ##Class com as instansias para acessar a api
     def initialize
         @uri = URI.parse("https://api.gotit.ai/Translation/v1.1/Translate")
         @request = Net::HTTP::Post.new(@uri, 'Content-Type' => 'application/json')
         @request.basic_auth("1989-ig1p4g5l", "rfNFuAOLnwX+VREC16Yp3Pp9E4/LmWT0tPwm/J0x")
     end
-
+    ## Metodo que recebe os dados do usuario para realizar a tradução
     def traduzir(mensagem, idioma_atual, idioma_traduz)
         @request.body = {T: mensagem, SL: idioma_atual, TL: idioma_traduz}.to_json
         response = Net::HTTP.start(@uri.hostname, @uri.port, use_ssl: @uri.scheme == "https") do |http|
             http.request(@request)
         end
-
+    ## analisa o conteúdo com formato JSON
         requisicao = JSON.parse(@request.body)
         resposta = JSON.parse(response.body)
-        save(requisicao, resposta)
-        
-        return resposta["result"]
-        
-    end
-
-    def save(request, response)
+        resposta["result"]
+    ## Formato em que o JSON vai ficar
         time = Time.new
-        idioma_atual = request["SL"]
-        mensagem = request["T"]
-        idioma_traduz = request["TL"]
-        traducao = response["result"]
+        idioma_atual = requisicao["SL"]
+        mensagem = requisicao["T"]
+        idioma_traduz = requisicao["TL"]
+        traducao = resposta["result"]
 
-        file = File.open(time.strftime("%m-%d-%Y.%H.%M.%S") + ".txt", 'w') do |linearq|
-            linearq.puts ("Idioma da mensagem: #{idioma_atual}")
-            linearq.puts ("Mensagem: #{mensagem}")
-            linearq.puts ("Idioma para tradução: #{idioma_traduz}")
-            linearq.puts ("Tradução: #{traducao}")
+        file = File.open(time.strftime("#{traducao}--%m-%d-%Y.%H.%M.%S") + ".txt", 'w') do |linearq|
+            linearq.puts "Idioma da mensagem: #{idioma_atual}"
+            linearq.puts "Mensagem: #{mensagem}"
+            linearq.puts "Idioma para tradução: #{idioma_traduz}"
+            linearq.puts "Tradução: #{traducao}"
         end
     end
 end
